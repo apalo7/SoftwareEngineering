37103d72bc7a55bca55d7823f589be6bed175f8d
Azad Palo <blauwin@macbookpro.home>
2026-03-04 19:06:41 +0000

    S309918 - Home page has been added

--- league/views.py
+++ league/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from league.models import Clubs
+
+
 # Create your views here.
+
+def home(request):
+    clubs = Clubs.objects.all()
+    return render(request, 'league/home.html', {'clubs': clubs})
