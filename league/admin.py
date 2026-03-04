363317784b5030f41230fda9249b89a683cb6c22
Azad Palo <blauwin@macbookpro.home>
2026-03-04 18:49:10 +0000

    S309918 - Admin account created and tables has been added

--- league/admin.py
+++ league/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from league.models import Clubs, Players, Matches, ClubSeasonStats, Profile
+
 # Register your models here.
+
+admin.site.register(Clubs)
+admin.site.register(Players)
+admin.site.register(Matches)
+admin.site.register(ClubSeasonStats)
+admin.site.register(Profile)
--- league/models.py
+++ league/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Clubs(models.Model):
+    club_name = models.CharField(max_length=100)
+    foundation_year = models.IntegerField()
+    club_colours = models.CharField(max_length=100)
+    coach_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.club_name
+
+
+class Players(models.Model):
+    club = models.ForeignKey(Clubs, on_delete=models.CASCADE, related_name='players')
+    player_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    position = models.CharField(max_length=100)
+    market_value = models.BigIntegerField()
+
+    def __str__(self):
+        return self.player_name
+
+class Matches(models.Model):
+    home_club = models.ForeignKey(Clubs, on_delete=models.CASCADE, related_name='home_matches')
+    away_club = models.ForeignKey(Clubs, on_delete=models.CASCADE, related_name='away_matches')
+    match_date = models.DateField()
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.home_club} {self.home_score} - {self.away_club} {self.away_score}'
+
+
+class ClubSeasonStats(models.Model):
+    club = models.OneToOneField(Clubs, on_delete=models.CASCADE)
+    matches_played = models.IntegerField()
+    wins = models.IntegerField()
+    draws = models.IntegerField()
+    losses = models.IntegerField()
+    goals_scored = models.IntegerField()
+    goals_against = models.IntegerField()
+    goal_difference = models.IntegerField()
+    points = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.club.club_name} Stats"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favourite_clubs = models.ManyToManyField(Clubs, blank=True)
+
+    def __str__(self):
+        return self.user.username
