3e0fede4d1efbda1f520ed74a5d25daf60f54afa
Azad Palo <blauwin@macbookpro.home>
2026-03-07 20:02:18 +0000

    S309918 - Database has been loaded into application

--- league/views.py
+++ league/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from .models import Club, Profile, Match, Player
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from .models import Club, Profile, Match, Player
 
 @login_required(login_url='/login/')
 def home(request):
     all_clubs = Club.objects.all()
-    return render(request, 'league/home.html', {'all_clubs': all_clubs})
-
-
-from .models import Club, Profile
+    return render(request, 'league/home.html', {'club': all_clubs})
 
 @login_required(login_url='/login/')
 def assign_favourite(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
-    all_club = Club.objects.all()
+    all_clubs = Club.objects.all()
 
     if request.method == 'POST':
         club_id = request.POST.get('club_id')
@@ -25,36 +25,36 @@ def assign_favourite(request):
             profile.favourite_club.add(selected_club)
             return redirect('home')
 
-    return render(request, 'league/assign_favourite.html', {'all_club': all_club})
+    return render(request, 'league/assign_favourite.html', {'all_clubs': all_clubs})
 
 @login_required(login_url='/login/')
 def match_results(request):
     matches = Match.objects.all().order_by('-match_date')
-    return render(request, 'league/match_results.html', {'matches': matches})
+    return render(request, 'league/match_results.html', {'match': matches})
 
 @login_required(login_url='/login/')
 def league_standings(request):
-    all_club = Club.objects.all()
-    return render(request, 'league/standings.html', {'all_club': all_club})
+    all_clubs = Club.objects.all()
+    return render(request, 'league/standings.html', {'all_clubs': all_clubs})
 
 @login_required(login_url='/login/')
 def club_list(request):
-    all_club = Club.objects.all()
-    return render(request, 'league/clubs.html', {'all_club': all_club})
+    all_clubs = Club.objects.all()
+    return render(request, 'league/clubs.html', {'all_clubs': all_clubs})
 
 @login_required(login_url='/login/')
 def player_list(request):
-    all_player = Player.objects.all()
-    return render(request, 'league/players.html', {'all_player': all_player})
-
+    # HTML all_players bekliyor
+    all_players = Player.objects.all()
+    return render(request, 'league/players.html', {'all_players': all_players})
 
 @login_required(login_url='/login/')
 def transfer_center(request):
     if not request.user.is_superuser:
         return redirect('home')
 
-    all_player = Player.objects.all()
-    all_club = Club.objects.all()
+    all_players = Player.objects.all()
+    all_clubs = Club.objects.all()
 
     if request.method == 'POST':
         player_id = request.POST.get('player_id')
@@ -66,11 +66,11 @@ def transfer_center(request):
 
             player.club = new_club
             player.save()
-            return redirect('player_list')  # Transfer sonrası listeye atar
+            return redirect('player_list')
 
     return render(request, 'league/transfer_center.html', {
-        'all_player': all_player,
-        'all_club': all_club
+        'all_players': all_players,
+        'all_clubs': all_clubs
     })
 
 class UserLoginView(LoginView):
